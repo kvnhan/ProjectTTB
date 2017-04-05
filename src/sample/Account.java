a3b6d665c2d6e09e9bb8880a1edc4875a6187d2d
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-04-05 12:37:31 -0400

    In "Accounts" added functions for inbox & modified constructor to contain an inbox of application IDs

--- src/sample/Account.java
+++ src/sample/Account.java
@@ -1,6 +1,8 @@
 package sample;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Adonay on 4/3/2017.
@@ -9,11 +11,14 @@ public class Account implements Serializable{
     private String username;
     private int accessLevel;
     private boolean isLoggedIn;
+    private ArrayList<String> inbox = new ArrayList<>();
 
-    public Account(String usrnm, int accL){
+
+    public Account(String usrnm, int accL, ArrayList<String> inbox){
         username = usrnm;
         accessLevel = accL;
         isLoggedIn = false;
+        this.inbox = inbox;
     }
 
     public String getUsername() {
@@ -31,4 +36,22 @@ public class Account implements Serializable{
     public void setLoggedIn(boolean loggedIn) {
         isLoggedIn = loggedIn;
     }
+
+    //returns the number of applications in the worker's inbox
+    public int numberOfApplications(){
+        return inbox.size();
+    }
+    //adds a specific application to the inbox
+    public void addToInbox(String sf){
+        inbox.add(0, sf);
+    }
+    //removes a specific appliation from the inbox
+    public void removeFromInbox(int formID){
+        inbox.remove(formID);
+    }
+    //gets a specific application from the inbox
+    public String getNextForm(){
+        return inbox.get(0);
+    }
+
 }
