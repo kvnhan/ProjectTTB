eb387e9069d976bd8746f485c34bb5dde00a29da
lgstuehrmann <lgstuehrmann18@gmail.com>
2017-04-07 17:55:08 -0400

    testing new branch

--- src/sample/ApplicationReviewController.java
+++ src/sample/ApplicationReviewController.java
@@ -41,10 +41,10 @@ public class ApplicationReviewController extends DatabaseUtil{
     @FXML
     TextArea commentsField;
     //for when switching to this scene from inbox
-    @FXML
-    public void initialize(){
-        approve.setText(accountsUtil.getInbox());
-    }
+   // @FXML
+   // public void initialize(){
+    //    approve.setText(accountsUtil.getInbox());
+    //}
     /*      repID.setText(application.ID);
             registryNo.setText("0000");
             prodSource.setText("TEST");
@@ -90,7 +90,7 @@ public class ApplicationReviewController extends DatabaseUtil{
         String sql = "UPDATE ALCOHOL SET status = 'rejected', comments =" + comments + " WHERE id = apptoassgn";
         stm.executeUpdate(sql);
         //update inbox for worker
-        sql = "UPDATE REVIEWS SET " + w.getInbox().remove(apptoassgn) + " WHERE username = "+ w.getUsername() +" ";
+       // sql = "UPDATE REVIEWS SET " + w.getInbox().remove(apptoassgn) + " WHERE username = "+ w.getUsername() +" ";
         stm.executeUpdate(sql);
     }
 
