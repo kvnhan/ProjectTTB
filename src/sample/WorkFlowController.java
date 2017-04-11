477ff5eb85741c4400f0a7c85071f748b473e0a8
elsaluthi <emluthi@wpi.edu>
2017-04-10 21:23:16 -0400

    added the stuff from work flow to main

--- src/sample/WorkFlowController.java
+++ src/sample/WorkFlowController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 
 public class WorkFlowController {
-
+/*
     private FXMLLoader fxmlLoader;
     @FXML private Button back;
     @FXML private Button first;
@@ -20,4 +20,48 @@ public class WorkFlowController {
     public void firstButton(ActionEvent event){
         work.switchScene("ApplicationReview.fxml", "Application Review");
     }
+    */
+
+    private FXMLLoader fxmlLoader;
+    @FXML private Button back;
+    @FXML private Button first;
+    ScreenUtil work = new ScreenUtil();
+
+    /**
+     * Checks if the back button has been clicked and returns user to Main Menu.
+     * @param event ActionEvent representing a button press.
+     */
+    public void buttonClicked (ActionEvent event){
+        if(event.getSource() == back){
+            work.switchScene("MainMenu.fxml","Main Menu");
+        }
+    }
+
+    /**
+     * Switches to the ApplicationReview screen.
+     * @param event Button press representing the ViewFirst button.
+     */
+    public void firstButton(ActionEvent event){
+
+        work.switchScene("ApplicationReview.fxml", "Application Review");
+    }
+
+
+    /*public void roundRobin({
+        if (getUnassigForms().size() <= 10) {
+            ArrayList<String> forms = getUnassigForms();
+            for (i = 0; i < forms.size(); i++) {
+                Account worker = getSmallWorker();
+                addToInbox(worker, forms.get(i));
+            }
+        }
+        //adds forms to workers 10 at a time
+        else{
+            ArrayList<String> forms = getUnassigForms();
+            for (i = 0; i < forms.size(); i++) {
+                Account worker = getSmallWorker();
+                addToInbox(worker, forms.get(i));
+        }
+    }*/
+
 }
