9bc822cccc08474286d37b40d93d90f0c72f96f8
CHRIS-PC\Chris <ccedron@wpi.edu>
2017-04-26 18:48:20 -0400

    all the labels in the AppReview now show the information they should. working on adding checkboxes and storing the information of the comments and the checkboxes that were selected somewhere.

--- src/sample/ApplicationReviewController.java
+++ src/sample/ApplicationReviewController.java
@@ -6,28 +6,38 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import sun.java2d.pipe.AlphaPaintPipe;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 
-import java.lang.reflect.*;
+import java.io.File;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.sql.*;
-import java.text.Format;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 /**
  * Controller for Application Review Screen.
  */
 public class ApplicationReviewController{
     @FXML
-    private Button approve, reject, goBack, ReviewHelpButton, forwardApp;
+    Button approve, reject, goBack, forwardApp;
     @FXML
-    private TextField repID, registryNo, prodSource, prodType, address,  phoneNo, email, dateApp, nameApp;
-    @FXML private  TextArea commentsField;
-    @FXML private ChoiceBox acctToChoose;
+     Label RepID_Label, PlantID_Label, SerialNo_Label, PlantAddress_Label,
+            ApplicantName_Label, MaillingAddress_Label, Email_Label, PhoneNumber_Label,
+            BrandName_Label, FancyName_Label, Formula_Label, AlcoholContent_Label, NetContent_Label,
+            Origin_Label, Sulfite_Label, Varietal_Label, Appellation_Label, Vintage_Label, Bottler_Label,
+            pH_Label;
+    @FXML
+    TextArea commentsField;
+    @FXML
+    ChoiceBox acctToChoose;
+    @FXML
+    ImageView LabelImage;
 
     private DatabaseUtil dbUtil = new DatabaseUtil();
     private ScreenUtil screenUtil = new ScreenUtil();
@@ -46,35 +56,72 @@ public class ApplicationReviewController{
      * Initializes the ApplicationReview Screen.
      */
     public void initialize() throws SQLException{
+        boolean foundImage = false;
         listForms = dbUtil.searchFormWithGovId(dbUtil.getAccountAid(username));
         if(appReviewMode == 2){
-            listForms = new ArrayList<ApplicationData>();
+            listForms = new ArrayList<>();
             listForms.add(WorkFlowController.getRowChosen());
         }
-        numberOfApps = listForms.size();
         thisForm = listForms.get(0);
-        repID.setText(Integer.toString(thisForm.getRepid()));
-        registryNo.setText(Integer.toString(thisForm.getPermitNo()));
-        prodSource.setText(thisForm.getAssociatedAlcoholData().getSourceOfProduct());
-        prodType.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getAlcoholType()));
-        address.setText(thisForm.getAddress());
-        phoneNo.setText(thisForm.getPhoneNumber());
-        email.setText(thisForm.getEmail());
-        dateApp.setText(thisForm.getSubmittedDate().toString());
-        nameApp.setText(thisForm.getApplicantName());
 
+        RepID_Label.setText(String.valueOf(thisForm.getRepid()));
+        PlantID_Label.setText(String.valueOf(thisForm.getPermitNo()));
+        SerialNo_Label.setText(String.valueOf(thisForm.getSerial()));
+        PlantAddress_Label.setText(String.valueOf(thisForm.getAddress()));
+
+        ApplicantName_Label.setText(String.valueOf(thisForm.getApplicantName()));
+        MaillingAddress_Label.setText(String.valueOf(thisForm.getAddress()));
+        Email_Label.setText(String.valueOf(thisForm.getEmail()));
+        PhoneNumber_Label.setText(String.valueOf(thisForm.getPhoneNumber()));
+
+        BrandName_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getBrandName()));
+        FancyName_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getName()));
+        Formula_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getFormulas()));
+        AlcoholContent_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getAlchContent()));
+        NetContent_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getNetContent()));
+
+        Origin_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getOriginCode()));
+        Sulfite_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getSulfiteDescription()));
+        Varietal_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getGrapeVarietal()));
+        Appellation_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getAppellation()));
+        Vintage_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getWineVintage()));
+        Bottler_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getBottlersInfo()));
+
+        pH_Label.setText(String.valueOf(thisForm.getAssociatedAlcoholData().getPhLevel()));
+
+        try {
+            InputStream resource = ScreenUtil.class.getClassLoader().getResourceAsStream("labels/"  + thisForm.getAssociatedAlcoholData().getAid() + ".jfif");
+            LabelImage.setImage(new javafx.scene.image.Image(resource, 500.0, 0.0, true, true));
+            foundImage = true;
+        }
+        catch(NullPointerException nullPoint){
+            if(foundImage == false) {
+                try {
+                    String path = getPath();
+                    File file = new File(path + "/" + thisForm.getAssociatedAlcoholData().getAid() + ".jpg");
+                    javafx.scene.image.Image image = new javafx.scene.image.Image(file.toURI().toString());
+                    System.out.println(thisForm.getAssociatedAlcoholData().getAid());
+                    LabelImage.setImage(image);
+                } catch (Exception e) {
+                    InputStream resource = ScreenUtil.class.getClassLoader().getResourceAsStream("labels/imageUnavailable.jpg");
+                    LabelImage.setImage(new javafx.scene.image.Image(resource, 100.0, 0.0, true, true));
+                    System.out.println("Image Was Not Found For " + thisForm.getAssociatedAlcoholData().getBrandName() + "'s " + thisForm.getAssociatedAlcoholData().getName());
+                }
+            }
+        }
         acctsObservableList = FXCollections.observableArrayList();
         acctsFound = dbUtil.searchAccountWithUserType(1);
         for(int i = 0; i < acctsFound.size(); i++){
             acctsObservableList.add(acctsFound.get(i).getUsername());
         }
-        acctToChoose.setItems(acctsObservableList);
+        //acctToChoose.setItems(acctsObservableList);
+
     }
 
 
 
     @FXML
-    void setGoBack(ActionEvent event){
+    void setGoBack(){
         screenUtil.switchScene("WorkFlow.fxml", "Main Menu");
     }
 
@@ -141,5 +188,18 @@ public class ApplicationReviewController{
 
     public static void setAppReviewMode(int appReviewMode) {
         ApplicationReviewController.appReviewMode = appReviewMode;
+    }
+
+    public String getPath() throws UnsupportedEncodingException {
+        URL url = this.getClass().getProtectionDomain().getCodeSource().getLocation();
+        String jarPath = URLDecoder.decode(url.getFile(), "UTF-8");
+        String parentPath = new File(jarPath).getParentFile().getPath();
+
+        String fileSeparator = System.getProperty("file.separator");
+        String newDir = parentPath + fileSeparator + "images" + fileSeparator;
+
+        System.out.println(newDir);
+
+        return newDir;
     }
 }
