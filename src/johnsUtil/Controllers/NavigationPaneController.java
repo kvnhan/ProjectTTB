f058f77d185bbfeb8f0817660ed8bb9a2b726487
Johnathan A`Vant <jaavant@wpi.edu>
2017-05-01 06:23:38 -0400

    logout and ui

--- src/johnsUtil/Controllers/NavigationPaneController.java
+++ src/johnsUtil/Controllers/NavigationPaneController.java
@@ -4,14 +4,18 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import johnsUtil.Main;
 import johnsUtil.model.SharedResources.Account;
 import johnsUtil.model.SharedResources.Screen;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -85,7 +89,17 @@ public class NavigationPaneController implements Initializable {
         logoutBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                Screen.getInstance().createConfirmBox("Test 123", "Test 345");
+                if(Screen.getInstance().createConfirmBox("Logout", "Are you sure you want to logout, all of your unsaved progress will be lose?")){
+                    Account.getInstance().logout();
+                    Stage primaryStage = Account.getInstance().getWindow();
+                    Parent root = null;
+                    try {
+                        root = FXMLLoader.load(getClass().getClassLoader().getResource("johnsUtil/Views/MainMenu.fxml"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    primaryStage.getScene().setRoot(root);
+                }
             }
         });
 
